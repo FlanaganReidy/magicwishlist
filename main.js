1510920f2e6297e53e1365f24a7858fcf6687a58
Shannon <reidy.pf@gmail.com>
2017-08-11 14:03:21 -0500

    deletes search field after you pring wishlist, wishlist now prints images, budget input is right aligned

--- main.js
+++ main.js
@@ -10,13 +10,18 @@ submit.addEventListener('click', function() {
 
 let printWish = document.querySelector('.listPrint');
 printWish.addEventListener('click', function() {
+  document.querySelector('.container').innerHTML = '';
   let thelist = document.querySelector('.wishList');
   thelist.innerHTML = '';
   let wishListTitle = document.querySelector('.wishListTitle');
   wishListTitle.innerHTML = `<h1>Wish List</h1>`
   wishlist.forEach(function(e) {
     let listItem = document.createElement('li');
-    listItem.innerHTML = e.name;
+    let cardinfo = `
+          <hr>
+          <img src=${e.image_uri}>
+          <p>Dollars: ${e.usd} | Tix: ${e.tix}</p>`
+    listItem.innerHTML = cardinfo;
     console.log(listItem)
     console.log(thelist)
     thelist.appendChild(listItem);
@@ -30,42 +35,42 @@ checkbudget.addEventListener('click', function() {
   checkingbudget(input.value, wishlist);
 })
 
-function searching(input) {
-  let card = fuzzify(input);
-  fetch("https://api.scryfall.com/cards/named?fuzzy=" + card + "&format=json")
-    .then(
-      function(response) {
-        if (response.status !== 200) {
-          if (response.status === 404) {
-            let thebody = document.querySelector(".container");
-            thebody.innerHTML = `<p>Either not a card or too many cards. Try again, and please, be a little more specific.</p>`
-          }
-          console.log(response.status);
-          return;
-        }
-        return response.json();
-
-      }).then(function(data) {
-      console.log(data);
-      let newCard = document.createElement('div');
-      newCard.className = "fetchedCards";
-      document.querySelector('.container').appendChild(newCard);
-      let cardinfo = `
-    <img src=${data.image_uri}>
-    <p>Dollars: ${data.usd} | Tix: ${data.tix}</p>`
-      newCard.innerHTML = cardinfo;
-      let saveButton = document.createElement('button');
-      newCard.appendChild(saveButton);
-      saveButton.innerHTML = "Save Me For Later!";
-      saveButton.className = "saveButton";
-      saveButton.addEventListener('click', function() {
-        wishlist.push(data);
-        saveButton.innerHTML = "You Saved This Card!";
-        console.log(wishlist);
-      });
-
-    });
-}
+// function searching(input) {
+//   let card = fuzzify(input);
+//   fetch("https://api.scryfall.com/cards/named?fuzzy=" + card + "&format=json")
+//     .then(
+//       function(response) {
+//         if (response.status !== 200) {
+//           if (response.status === 404) {
+//             let thebody = document.querySelector(".container");
+//             thebody.innerHTML = `<p>Either not a card or too many cards. Try again, and please, be a little more specific.</p>`
+//           }
+//           console.log(response.status);
+//           return;
+//         }
+//         return response.json();
+//
+//       }).then(function(data) {
+//       console.log(data);
+//       let newCard = document.createElement('div');
+//       newCard.className = "fetchedCards";
+//       document.querySelector('.container').appendChild(newCard);
+//       let cardinfo = `
+//     <img src=${data.image_uri}>
+//     <p>Dollars: ${data.usd} | Tix: ${data.tix}</p>`
+//       newCard.innerHTML = cardinfo;
+//       let saveButton = document.createElement('button');
+//       newCard.appendChild(saveButton);
+//       saveButton.innerHTML = "Save Me For Later!";
+//       saveButton.className = "saveButton";
+//       saveButton.addEventListener('click', function() {
+//         wishlist.push(data);
+//         saveButton.innerHTML = "You Saved This Card!";
+//         console.log(wishlist);
+//       });
+//
+//     });
+// }
 
 function fuzzify(input) {
   let betterinput = input.replace(/ /i, "+");
@@ -109,6 +114,7 @@ function greaterSearch(input) {
             }
             return response.json();
           }).then(function(data) {
+            console.log(data);
             let newCard = document.createElement('div');
             newCard.className = "fetchedCards";
             document.querySelector('.container').appendChild(newCard);
